0c55a47c8d9b0fe129d27ba124e61e436c8663f3
FOBABS <fobabs2win@rocketmail.com>
2025-05-22 14:30:53 +0100

    test: check for deployment and `addRewardPoints`

--- test/LootBox.t.sol
+++ test/LootBox.t.sol
@@ -13,6 +13,8 @@ import {Constants} from "../src/utils/Constants.sol";
 contract LootBoxTest is Constants, Test {
     LootBox public lootBox;
     HelperConfig public helperConfig;
+    ERC20Mock public erc20Token;
+    ERC721Mock public erc721Token;
 
     uint256 public openFee;
     address public vrfCoordinator;
@@ -28,11 +30,35 @@ contract LootBoxTest is Constants, Test {
         (lootBox, helperConfig) = deployer.run();
         vm.deal(player, FUND_AMOUNT);
 
+        erc20Token = new ERC20Mock();
+        erc721Token = new ERC721Mock();
+
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
         openFee = config.openFee;
         vrfCoordinator = config.vrfCoordinator;
         subscriptionId = config.subscriptionId;
         keyHash = config.keyHash;
         callbackGasLimit = config.callbackGasLimit;
+    }
+
+    function test_Deployment() public view {
+        assertEq(lootBox.getOpenFee(), openFee);
+    }
+
+    // function test_ExpectEmitOnAddReward() public {
+    //     vm.expectEmit(true, false, false, true);
+    //     emit LootBox.RewardAdded(0, LootBox.RewardType.POINTS, address(0), 100, 50);
+    //     vm.prank(lootBox.owner());
+    //     lootBox.addReward(LootBox.RewardType.POINTS, address(0), 100, 50);
+    // }
+
+    function test_AddRewardPoints() public {
+        vm.prank(lootBox.owner());
+        lootBox.addReward(LootBox.RewardType.POINTS, address(0), 100, 50);
+        LootBox.Reward memory rewards = lootBox.getRewards(0);
+        assertEq(uint8(rewards.rewardType), uint8(LootBox.RewardType.POINTS));
+        assertEq(rewards.tokenAddress, address(0));
+        assertEq(rewards.amount, 100);
+        assertEq(rewards.weight, 50);
     }
 }
