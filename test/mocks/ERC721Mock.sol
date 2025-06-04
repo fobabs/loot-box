da0f12ff783b4be8f9cadcbf42b1c9671f8615f4
FOBABS <fobabs2win@rocketmail.com>
2025-06-04 11:22:55 +0100

    feat(ERC721Mock): add mint function

--- test/mocks/ERC721Mock.sol
+++ test/mocks/ERC721Mock.sol
@@ -5,4 +5,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ERC721Mock is ERC721 {
     constructor() ERC721("TestNFT", "TNFT") {}
+
+    function mint(address account, uint256 tokenId) external {
+        _mint(account, tokenId);
+    }
 }
